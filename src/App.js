6ff4e913c62151fa0d21affa49b44669d49a7b4e
AMINE1921 <amineoubaya1921@gmail.com>
2020-09-21 22:32:34 +0200

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HashRouter as Route, Switch} from 'react-router-dom';
+import {HashRouter, Route, Switch} from 'react-router-dom';
 import home from './pages/home';
 import contact from './pages/contact';
 import portfolio from './pages/portfolio';
@@ -9,6 +9,7 @@ import notFound from './pages/notFound';
 const App = () => {
   return (
     <>
+      <HashRouter>
         <Switch>
           <Route path="/" exact component={home} />
           <Route path="/competences" component={knowledges} />
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/portfolio" component={portfolio} />
           <Route component={notFound} />
         </Switch>
+      </HashRouter>
     </>
   );
 };
