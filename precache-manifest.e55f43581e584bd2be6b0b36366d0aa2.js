9ca8ac9ff1ee826837b500801c88d23825782a91
AMINE1921 <amineoubaya1921@gmail.com>
2020-09-21 21:48:58 +0200

    Updates

---
+++ precache-manifest.e55f43581e584bd2be6b0b36366d0aa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3115fc66035c39b44f0deff6a822af73",
+    "url": "/ml-website/index.html"
+  },
+  {
+    "revision": "af61eb78ae002bc4331b",
+    "url": "/ml-website/static/js/2.62154661.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ml-website/static/js/2.62154661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044e8eac89704623e36e",
+    "url": "/ml-website/static/js/main.6100e081.chunk.js"
+  },
+  {
+    "revision": "ad3de8461b5594a04cf5",
+    "url": "/ml-website/static/js/runtime-main.f7f3c004.js"
+  }
+]);
