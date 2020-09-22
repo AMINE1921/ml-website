e94986facd980c4560d55dee5d2bb0dadd3a389e
AMINE1921 <amineoubaya1921@gmail.com>
2020-09-22 20:17:05 +0200

    Updates

---
+++ precache-manifest.7af03fb7694ba12143959d80f74dd1c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfeebfa8c6d73e06d09a4506f3ef135",
+    "url": "/ml-website/index.html"
+  },
+  {
+    "revision": "d6297dc17c2c78505e21",
+    "url": "/ml-website/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cb8781ecb927c8cf68ee",
+    "url": "/ml-website/static/css/main.b1381715.chunk.css"
+  },
+  {
+    "revision": "d6297dc17c2c78505e21",
+    "url": "/ml-website/static/js/2.c415f823.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/ml-website/static/js/2.c415f823.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb8781ecb927c8cf68ee",
+    "url": "/ml-website/static/js/main.46dbf765.chunk.js"
+  },
+  {
+    "revision": "ad3de8461b5594a04cf5",
+    "url": "/ml-website/static/js/runtime-main.f7f3c004.js"
+  },
+  {
+    "revision": "95309e2fe0a16b4aed4e280f8b6f8bdd",
+    "url": "/ml-website/static/media/ml.95309e2f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ml-website/precache-manifest.ea3fdebbb778f646de45492a6f447776.js"
+  "/ml-website/precache-manifest.7af03fb7694ba12143959d80f74dd1c2.js"
 );
 
 self.addEventListener('message', (event) => {
