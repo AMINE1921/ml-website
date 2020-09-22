eaa8ab6e1fe00f93bacc5fb72a0e9ecb24da2f0b
AMINE1921 <amineoubaya1921@gmail.com>
2020-09-22 20:16:13 +0200

    finished header

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import * as ReactBootstrap from 'react-bootstrap';
+import $ from 'jquery';
 import { NavLink } from 'react-router-dom';
-import andyCapp from '../media/andyCapp.png'
+import ml_logo from '../media/ml.png'
+
+function handleCollapse(event) {
+        event.preventDefault();
+        if( $('body').hasClass('menu-show') ) {
+            $('body').removeClass('menu-show');
+            $('#main-nav > .js-nav-toggle').removeClass('show');
+        } else {
+            $('body').addClass('menu-show');
+            setTimeout(function(){
+                $('#main-nav > .js-nav-toggle').addClass('show');
+            }, 900);
+        }
+}
+
+function isCurrentUrl(url) {
+    const { hash } = document.location;
+    return Array.isArray(url) ? url.some(u => hash === (u)) : hash === (url);
+}
 
 const Navigation = () => {
     return (
@@ -11,22 +29,28 @@ const Navigation = () => {
                     <div class="row">
                         <div class="col-md-12">
                             <div class="navbar-brand name-logo">
-                                <a class="logo" href="/"><span class="logo-img" style={{backgroundImage: `url(${andyCapp})`}}> </span>ANDY CAPP</a>
+                                <div class="logo"><NavLink exact to="/" ><span class="logo-img" style={{backgroundImage: `url(${ml_logo})`}}> </span>Mohamed Lamine OUBAYA</NavLink></div>
                             </div>
-                            <a className="colorlib-nav-toggle"><i></i></a>
+                            <div className="js-nav-toggle nav-toggle" onClick={handleCollapse.bind(this)}><i></i></div>
                         </div>
                     </div>
                 </div>
             </header>
 
-            <nav>
-                <div class="col-md-12 text-center">
-                    <ul className="nav-links">
-                        <li><NavLink exact to="/" activeClassName="navActive"><span><small>01 </small>Accueil</span></NavLink></li>
-                        <li><NavLink exact to="/competences" activeClassName="navActive"><span><small>02 </small>Compétences</span></NavLink></li>
-                        <li><NavLink exact to="/portfolio" activeClassName="navActive"><span><small>03 </small>Portfolio</span></NavLink></li>
-                        <li><NavLink exact to="/contact" activeClassName="navActive"><span><small>04 </small>Contact</span></NavLink></li>
-                    </ul>
+            <nav id="main-nav" role="navigation">
+                <div className="js-fullheight col-table">
+                    <div className="col-table-cell js-fullheight">
+                        <div className="row no-gutters">
+                            <div className="col-md-12 text-center">
+                                <ul>
+                                    <li className={`${isCurrentUrl('#/') ? 'active' : ''}`} onClick={handleCollapse.bind(this)}><NavLink exact to="/" ><span><small>01</small>Accueil</span></NavLink></li>
+                                    <li className={`${isCurrentUrl('#/competences') ? 'active' : ''}`} onClick={handleCollapse.bind(this)}><NavLink exact to="/competences" ><span><small>02</small>Compétences</span></NavLink></li>
+                                    <li className={`${isCurrentUrl('#/portfolio') ? 'active' : ''}`} onClick={handleCollapse.bind(this)}><NavLink exact to="/portfolio" ><span><small>03</small>Portfolio</span></NavLink></li>
+                                    <li className={`${isCurrentUrl('#/contact') ? 'active' : ''}`} onClick={handleCollapse.bind(this)}><NavLink exact to="/contact" ><span><small>04</small>Contact</span></NavLink></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </nav>
         </div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -10,8 +10,8 @@ const home = () => {
                 <div class="container">
                     <div class="row slider-text align-items-center text-center justify-content-center" data-scrollax-parent="true">
                         <div class="col-md-12 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
-                            <p><a href="#" class="scroll">SALUT JE SUIS</a></p>
-                            <h1 class="mb-3" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Andy Capp</h1>
+                            <p>SALUT JE SUIS</p>
+                            <h1 class="mb-3" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Mohamed Lamine OUBAYA</h1>
                         </div>
                     </div>
                 </div>
