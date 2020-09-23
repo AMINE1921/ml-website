cfa9d21ea4b8ac530d604da8309ec955c804ad03
AMINE1921 <amineoubaya1921@gmail.com>
2020-09-23 14:49:37 +0200

    fix Modal

--- src/components/popUp.js
+++ src/components/popUp.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {Button, Modal} from 'react-bootstrap';
+import {isMobile} from 'react-device-detect';
 import DisplayPDF from "./DisplayPDF";
 import cv from '../media/CV.pdf'
 
@@ -11,12 +12,13 @@ function Popup() {
         return (
             <div>
                 <div className="btn-download">
+                  { isMobile ? <div>
                     <Button className="btn-cv" variant="outline-light" onClick={showing}>Regarder le CV</Button>
                     <Modal id="modalCV" size="lg" show={show} onHide={showing}>
-                        <Modal.Header closeButton></Modal.Header>
+                        {/* <Modal.Header closeButton></Modal.Header> */}
                         <Modal.Body><DisplayPDF pdf={cv} /></Modal.Body>
-                    </Modal>
-                    <a className="btn-cv btn btn-outline-light" href={cv} download="CV_MohamedLamine_OUBAYA">Télécharger le CV</a>
+                    </Modal> </div> :
+                    <a className="btn-cv btn btn-outline-light" href={cv} download="CV_MohamedLamine_OUBAYA">Télécharger le CV</a> }
                 </div>
                 
             </div>
