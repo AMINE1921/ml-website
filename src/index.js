4d1b2fc03578aa987f999a0475f318faa76ee458
AMINE1921 <amineoubaya1921@gmail.com>
2021-07-13 17:10:18 +0200

    adding loading animation

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/style.scss";
+import { Spinner } from "react-bootstrap";
 
 import "./i18nextConf";
 
 ReactDOM.render(
-  <Suspense fallback="...">
+  <Suspense
+    fallback={<Spinner animation="border" variant="light" role="status" />}
+  >
     <React.StrictMode>
       <App />
     </React.StrictMode>
