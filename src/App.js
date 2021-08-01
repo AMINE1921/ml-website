40c49e84cc45a0b2fab13b830e732cc9ed33a0b4
AMINE1921 <amineoubaya1921@gmail.com>
2021-08-01 04:22:18 +0200

    update contact page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import {HashRouter, Route, Switch} from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Portfolio from './pages/Portfolio';
-import Knowledges from './pages/Knowledges';
-import NotFound from './pages/NotFound';
+import React from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Knowledges from "./pages/Knowledges";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
@@ -20,6 +22,7 @@ const App = () => {
           <Route component={NotFound} />
         </Switch>
       </HashRouter>
+      <ToastContainer />
     </>
   );
 };
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,18 +1,122 @@
-import React from 'react';
-import Navigation from '../components/Navigation/Navigation';
+import React from "react";
+import Navigation from "../components/Navigation/Navigation";
+import { Form, Button } from "react-bootstrap";
+import emailjs from "emailjs-com";
+import { toast } from "react-toastify";
+import isEmpty from "../utils/isEmpty";
+import { useTranslation } from "react-i18next";
 
 const Contact = () => {
-    return (
-        <div id="contact">
-            <Navigation></Navigation>
-            <div className="container contact">
-                <a href="mailto:mohamedlamine.oubaya@gmail.com"><i className="fas fa-envelope fa-5x"></i></a>
-                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/mohamed-lamine-oubaya/"><i className="fab fa-linkedin fa-5x"></i></a>
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/AMINE1921"><i className="fab fa-github-square fa-5x"></i></a>
-                <a target="_blank" rel="noopener noreferrer" href="http://discordapp.com/channels/@AMINE#5328"><i className="fab fa-discord fa-5x"></i></a>
-            </div>
-        </div>
-    );
+  const { t } = useTranslation();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    if (
+      isEmpty(e.target.name.value) ||
+      isEmpty(e.target.email.value) ||
+      isEmpty(e.target.subject.value) ||
+      isEmpty(e.target.message.value)
+    ) {
+      toast.warning(t("invalid_message"), {
+        position: "bottom-right",
+        autoClose: 3000,
+        draggable: true,
+        closeOnClick: true,
+        hideProgressBar: false,
+      });
+    } else {
+      emailjs
+        .sendForm(
+          "service_s0ayrn8",
+          "template_s7czwgt",
+          e.target,
+          "user_9bY5S31Mlhu3gGlRYg3CT"
+        )
+        .then(
+          (result) => {
+            toast.success(t("success_message"), {
+              position: "bottom-right",
+              autoClose: 3000,
+              draggable: true,
+              closeOnClick: true,
+              hideProgressBar: false,
+            });
+          },
+          (error) => {
+            toast.error(t("error_message"), {
+              position: "bottom-right",
+              autoClose: 3000,
+              draggable: true,
+              closeOnClick: true,
+              hideProgressBar: false,
+            });
+          }
+        );
+      e.target.reset();
+    }
+  };
+
+  return (
+    <div id="contact">
+      <Navigation></Navigation>
+      <Form className="container" onSubmit={sendEmail}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Control type="hidden" name="contact_number" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea2">
+          <Form.Label>{t("name")}</Form.Label>
+          <Form.Control type="text" name="name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>{t("email")}</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="name@example.com"
+            name="email"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea3">
+          <Form.Label>{t("subject")}</Form.Label>
+          <Form.Control type="text" name="subject" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea4">
+          <Form.Label>{t("message")}</Form.Label>
+          <Form.Control as="textarea" rows={3} name="message" />
+        </Form.Group>
+        <Form.Group className="col text-center" controlId="exampleForm.submit">
+          <Button variant="outline-success" type="submit">
+            {t("submit")}
+          </Button>
+        </Form.Group>
+      </Form>
+
+      <div className="container contact">
+        <a href="mailto:mohamedlamine.oubaya@gmail.com">
+          <i className="fas fa-envelope fa-2x"></i>
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/mohamed-lamine-oubaya/"
+        >
+          <i className="fab fa-linkedin fa-2x"></i>
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/AMINE1921"
+        >
+          <i className="fab fa-github-square fa-2x"></i>
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://discordapp.com/channels/@AMINE#5328"
+        >
+          <i className="fab fa-discord fa-2x"></i>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
